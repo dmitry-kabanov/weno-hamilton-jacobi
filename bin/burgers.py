d74c82d31071d80c5433fce0ebc46a1145b00d7e
Dmitry Kabanov <kabanovdmitry@gmail.com>
2014-06-30 17:06:42 +0400

    Refactor get_alpha() to make it faster
    Computations of alpha using for loop are very slow. I switch to usage
    of element-wise numpy functions to make get_alpha() function faster.

--- bin/burgers.py
+++ bin/burgers.py
@@ -4,15 +4,10 @@ import matplotlib.pyplot as plt
 
 
 def get_alpha(x, t, u, u_x_plus, u_x_minus):
-    max = np.zeros_like(x)
+    f1 = np.abs(u_x_plus + 1.0)
+    f2 = np.abs(u_x_minus + 1.0)
 
-    for i in range(len(x)):
-        if np.abs(u_x_plus[i] + 1.0) > np.abs(u_x_minus[i] + 1.0):
-            max[i] = np.abs(u_x_plus[i] + 1.0)
-        else:
-            max[i] = np.abs(u_x_minus[i] + 1.0)
-
-    return max
+    return np.maximum(f1, f2)
 
 
 def flux(x, t, u, u_x):
